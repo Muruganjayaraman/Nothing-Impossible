4c44dc5a6ccf39dfb0c7c041d2a74b4679ab3a7d
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-12 16:28:27 +0530

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,20 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Three {
+	public static void main(String args[]){
+		char ch;
+		Scanner in=new Scanner(System.in);
+		ch=in.next().charAt(0);
+		if(ch=='a'|ch=='e'|ch=='i'|ch=='o'|ch=='u'|ch=='A'|ch=='E'|ch=='I'|ch=='O'|ch=='U'){
+			System.out.println("Vowel");
+		}
+		else if((ch>='a'&&ch<='z')|(ch>='A'&&ch<='Z')){
+			System.out.println("Constant");
+		}
+	else{
+		System.out.println("Invalid");
+	}
+	}
+}
