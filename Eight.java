0aa9868fc37289ba41638b43cccafa0ee8cc00b8
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-12 18:52:04 +0530

    Add files via upload

---
+++ Eight.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Eight {
+	public static void main(String args[]){
+		int a,sum=0;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		for(int i=1;i<=a;i++){
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
