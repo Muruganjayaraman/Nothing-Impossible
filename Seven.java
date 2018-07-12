0090a29dd902c98ea36cb4d143279fddc3bedc80
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-12 18:25:49 +0530

    Add files via upload

---
+++ Seven.java
@@ -0,0 +1,16 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Seven {
+	public static void main(String args[]){
+		int b;
+		String a;
+		Scanner in=new Scanner(System.in);
+		b=in.nextInt();
+		a=in.next();
+		for(int i=0;i<b;i++){
+			System.out.println(a);
+		}
+	}
+}
