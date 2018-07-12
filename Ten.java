e5831c431697e961616715c3c77e89eab26d0ab7
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-12 19:24:46 +0530

    Add files via upload

---
+++ Ten.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Ten {
+	public static void main(String args[]){
+		int a;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		String b=Integer.toString(a);
+		int c=b.length();
+		System.out.println(c);
+}
+}
