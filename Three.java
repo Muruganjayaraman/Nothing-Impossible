769daa587d13de21fd8a4a3c17369b019162bbb0
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-12 16:34:45 +0530

    Add files via upload

--- Three.java
+++ Three.java
@@ -7,14 +7,11 @@ public class Three {
 		char ch;
 		Scanner in=new Scanner(System.in);
 		ch=in.next().charAt(0);
-		if(ch=='a'|ch=='e'|ch=='i'|ch=='o'|ch=='u'|ch=='A'|ch=='E'|ch=='I'|ch=='O'|ch=='U'){
-			System.out.println("Vowel");
-		}
-		else if((ch>='a'&&ch<='z')|(ch>='A'&&ch<='Z')){
-			System.out.println("Consonant");
+		if((ch>='a'&&ch<='z')|(ch>='A'&&ch<='Z')){
+			System.out.println("Yes");
 		}
 	else{
-		System.out.println("Invalid");
+		System.out.println("No");
 	}
 	}
 }
