600f2eb21d7b52876e107d79f90c419dc1aebbda
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-12 16:37:40 +0530

    Add files via upload

---
+++ Four.java
@@ -0,0 +1,17 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Four {
+	public static void main(String args[]){
+		char ch;
+		Scanner in=new Scanner(System.in);
+		ch=in.next().charAt(0);
+		if((ch>='a'&&ch<='z')|(ch>='A'&&ch<='Z')){
+			System.out.println("Yes");
+		}
+	else{
+		System.out.println("No");
+	}
+	}
+}
