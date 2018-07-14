69ed66eb7f628aacd77003f7d4f9bc484888e881
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-14 12:20:42 +0530

    Prime or not

---
+++ Prime.java
@@ -0,0 +1,26 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,i,count=0;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		for(i=2;i<a;i++){
+				if(a%i==0){
+					count++;
+			}
+		}
+		if(count==0){
+			System.out.println("yes");
+		}
+		else{
+			System.out.println("no");
+		}
+		
+	}
+
+}
