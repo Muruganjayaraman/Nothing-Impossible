201c80a49165374ca0ab61eb059fa70a2319f98b
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-21 19:38:23 +0530

    Swapping using Bitwise

---
+++ SwapBitwise.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SwapBitwise {
+	public static void main(String args[]){
+		int a,b;
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println(a+" "+b);
+	}
+
+}
