14ba5b0acdc4a3899fd24e1193087140ed1e9456
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-21 17:18:04 +0530

    Count a string without string

---
+++ guvi2.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class guvi2 {
+	public static void main(String args[]){
+		String a;
+		int sum=0;
+		Scanner in=new Scanner(System.in);
+		a=in.nextLine();
+		String b[]=a.split(" ");
+		for(int i=0;i<b.length;i++){
+			sum=sum+b[i].length();
+		}
+		System.out.println(sum);
+		
+	}
+
+}
