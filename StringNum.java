12da8379b4820d86d2cb7a90f8e134c5b5e0efdb
Muruganjayaraman <36439931+Muruganjayaraman@users.noreply.github.com>
2018-07-22 09:22:55 +0530

    String a count  number

---
+++ StringNum.java
@@ -0,0 +1,17 @@
+package guvi2;
+
+import java.util.Scanner;
+
+public class StringNum {
+	public static void main(String args[]){
+		String a;
+		Scanner in=new Scanner(System.in);
+		a=in.next();
+		int b=in.nextInt();
+		for(int i=1;i<=b;i++){
+			System.out.println(a);
+		}
+		
+	}
+
+}
